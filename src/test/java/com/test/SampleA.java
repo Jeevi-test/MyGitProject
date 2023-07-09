8c03a2adbc76eb299dcdb5038d010999ca58b3ae
unknown <hlfjeevi@gmail.com>
2023-07-09 12:50:35 +0530

    I push my first project

---
+++ src/test/java/com/test/SampleA.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class SampleA {
+public static void main(String[] args) {
+	SampleA sa = new SampleA();
+	sa.method1();
+}
+	public void method1() {
+		System.out.println("Hello" + "\nJeevitha");
+		
+	}
+}
